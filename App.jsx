eb4781b3f8dbc88bd40159fbfcc0149ed2490fc4
HardikBajaj10 <hardik.bajaj@bcah.christuniversity.in>
2025-04-09 10:42:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Parent from './parent'
+import './App.css';
+
+function App() {
+  return (
+    <div classname="app-container"> 
+      <h2> COUNT BOX</h2>
+      <Parent />
+    </div>
+  );
+}
+   
+export default App;
